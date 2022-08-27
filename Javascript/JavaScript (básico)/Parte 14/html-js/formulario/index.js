f3eba414fc283dd3fa36df33fe45baf0ffa44fa2
Maximiliano Palavecino <maxipp042@gmail.com>
2022-08-26 22:32:45 -0300

    added events notes

---
+++ Javascript/JavaScript (básico)/Parte 14/html-js/formulario/index.js
@@ -0,0 +1,9 @@
+const f = document.getElementById('formulario')
+
+console.log(f)
+
+f.addEventListener("submit", event => {
+    console.log(event)
+
+    event.preventDefault()
+})
---
+++ Javascript/JavaScript (básico)/Parte 14/html-js/index.js
@@ -0,0 +1,12 @@
+const a = 4
+const b = 8
+
+console.log(a * b)
+console.log('Hola')
+
+const p = document.getElementById("parrafo")
+
+console.log(p)
+
+
+// bind() --> vincular
